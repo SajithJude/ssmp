aa275dbf54588bdeb7cbf16b3d589e5431b61811
Jude Sajith <judesajith.aj@gmail.com>
2023-04-21 00:51:57 +0530

    ss

--- app.py
+++ app.py
@@ -27,23 +27,16 @@ def generate_maw(wordes):
     syllables_list = [divide_syllables(word) for word in word_list]
 
     maws = []
-    syllables = []
     for i in range(len(syllables_list)):
         for j in range(i+1, len(syllables_list)+1):
             maw = "".join([syllables_list[k][0] for k in range(i,j)])
 
             if len(maw) >= 6 and len(maw) <= 20:
                 maws.append(maw)
-        
-        # Add the list of syllables for the current word to the syllables list
-        syllables.append(syllables_list[i])
-    
-    # Return both the list of MAWs and the list of syllables
-    return maws, syllables
-
+    return maws
    
 def app():
-    st.title("Memory Anchor Words Generator")
+    st.title("SMPL Memory Anchor Words Generator")
 
     # Text input for entering the words to memorize
     words_to_memorize = st.text_input("Enter the words to memorize, separated by commas")
@@ -54,15 +47,13 @@ def app():
             # Split the input words into a list
             word_list = [word.strip() for word in words_to_memorize.split(",")]
 
-            # Generate MAWs and syllables for the input words
-            maws, syllables_list = generate_maw(word_list)
+            # Generate MAWs for the input words
+            maws = generate_maw(word_list)
 
-            # Display the MAWs and syllables for each word
-            st.write("Memory Anchor Words and Syllables:")
-            for i in range(len(word_list)):
-                st.write(word_list[i])
-                st.write("Syllables:", syllables_list[i])
-                st.write("MAWs:", [maw for maw in maws if word_list[i] in maw])
+            # # Display the MAWs
+            st.subheader("Memory Anchor Words:")
+            for maw in maws:
+                st.write(maw)
         else:
             st.write("Please enter some words to memorize.")
 
