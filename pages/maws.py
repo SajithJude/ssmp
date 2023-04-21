1dea9913caafee703d6806f6c550d4ce835b533d
Jude Sajith <judesajith.aj@gmail.com>
2023-04-22 03:35:47 +0530

    sss

--- pages/maws.py
+++ pages/maws.py
@@ -1,39 +1,42 @@
+import itertools
 import streamlit as st
 import pyphen
 
-# Set language fallback and initialize Pyphen object
-# pyphen.language_fallback('nl_NL_variant1')
+# Initialize Pyphen object
 dic = pyphen.Pyphen(lang='en_US')
+
+# Function to generate syllable combinations for a word
+def syllable_combinations(word):
+    hyphenated_word = dic.iterate(word)
+    hyp = dic.inserted(word)
+    syllist = [hyp]
+    for variation in hyphenated_word:
+        vary = "-".join(variation)
+        syllist.append(vary)
+    return syllist
+
+# Function to generate MAWs
+def generate_maws(words):
+    syllable_combinations_list = [syllable_combinations(word) for word in words]
+    maw_combinations = list(itertools.product(*syllable_combinations_list))
+    maws = [''.join(maw) for maw in maw_combinations]
+    return maws[:5]
 
 # Define Streamlit app
 def app():
     # Add a title
-    st.title("Rule-based maws generator")
+    st.title("Memory Anchor Words (MAWs) Generator")
 
     # Add a text input field for the user to enter a sentence
     sentence = st.text_input("Enter a sentence:", "")
 
     # Check if the user has entered a sentence
     if sentence:
-        # Split the sentence into words
         words = sentence.split()
-
-        for word in words:
-            # Get the hyphenation pairs for the entered word
-            hyphenated_word = dic.iterate(word)
-            hyp = dic.inserted(word)
-            syllist = [hyp]
-
-            # Display the hyphenation variations for the current word
-            st.info(f"Syllable variations for the word **{word}**:")
-            st.write(hyp)
-            for variation in hyphenated_word:
-                vary = "-".join(variation)
-                st.write(str(vary))
-                syllist.append(str(vary))
-
-            # Display the list of syllable variations for the current word
-            st.write(syllist)
+        maws = generate_maws(words)
+        st.write("Suggested Memory Anchor Words:")
+        for maw in maws:
+            st.info(maw)
 
 # Run the Streamlit app
 if __name__ == '__main__':
