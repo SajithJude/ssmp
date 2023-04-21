6c4f04566322ef31cd8c92cb78589ae8070e4b81
Jude Sajith <judesajith.aj@gmail.com>
2023-04-22 03:46:26 +0530

    ssss

--- pages/maws.py
+++ pages/maws.py
@@ -24,12 +24,12 @@ def syllable_combinations(word):
 # Function to generate MAWs
 def generate_maws(words):
     syllable_combinations_list = [
-        [syllable[:3] for syllable in syllable_combinations(word)]
+        [syllable[:4] for syllable in syllable_combinations(word)]
         for word in words
     ]
     maw_combinations = list(itertools.product(*syllable_combinations_list))
     maws = [''.join(maw) for maw in maw_combinations]
-    return maws[:5]
+    return maws
 
 # Define Streamlit app
 def app():
