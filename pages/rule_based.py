8593f422e04b612040058bd096f24c2a890724e4
Jude Sajith <judesajith.aj@gmail.com>
2023-04-23 00:38:47 +0530

    ssss

--- pages/rule_based.py
+++ pages/rule_based.py
@@ -3,6 +3,7 @@ import pyphen
 
 dic = pyphen.Pyphen(lang='en_US')
 
+
 def split_syllables(word):
     vowels = 'aeiouy'
     splits = []
@@ -11,16 +12,19 @@ def split_syllables(word):
 
     while i < len(word) - 1:
         if word[i] in vowels and word[i+1] not in vowels:
-            split = word[:i+1] + '-' + word[i+1:]
+            split = word[:i] + '-' + word[i:]
             if len(split.split('-')[0]) > 2:
                 splits.append(split)
-        if word[i] not in vowels and word[i+1] in vowels:
-            split = word[:i+1] + '-' + word[i+1:]
+                i += 1  # skip the hyphen
+        elif word[i] not in vowels and word[i+1] in vowels:
+            split = word[:i] + '-' + word[i:]
             if len(split.split('-')[0]) > 2:
                 splits.append(split)
+                i += 1  # skip the hyphen
         i += 1
 
     return list(set(splits))
+
 
 
 def app():
