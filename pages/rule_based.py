e7dc4d30196ad5539b9e9915644b183ee090a395
Jude Sajith <judesajith.aj@gmail.com>
2023-04-23 00:23:56 +0530

    ssss

--- pages/rule_based.py
+++ pages/rule_based.py
@@ -12,15 +12,16 @@ def split_syllables(word):
     while i < len(word) - 1:
         if word[i] in vowels and word[i+1] not in vowels:
             split = word[:i+1] + '-' + word[i+1:]
-            if len(split.split('-')[0]) > 2:
+            if len(split.split('-')[0]) > 2 and len(split.split('-')[1]) > 3:
                 splits.append(split)
         if word[i] not in vowels and word[i+1] in vowels:
             split = word[:i+1] + '-' + word[i+1:]
-            if len(split.split('-')[0]) > 2:
+            if len(split.split('-')[0]) > 2 and len(split.split('-')[1]) > 3:
                 splits.append(split)
         i += 1
 
     return list(set(splits))
+
 
 
 def app():
