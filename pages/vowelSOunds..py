7784d94bc1c83c80b043f3b295263e5f11a88df1
Jude Sajith <judesajith.aj@gmail.com>
2023-04-23 00:08:02 +0530

    sss

--- pages/vowelSOunds..py
+++ pages/vowelSOunds..py
@@ -9,25 +9,37 @@ cmu_dict = cmudict.dict()
 def is_vowel_sound(phoneme):
     return any(char.isdigit() for char in phoneme)
 
+def phonemes_to_letters(syllables):
+    letters = ''
+    for syllable in syllables:
+        for phoneme in syllable:
+            if phoneme[-1].isdigit():
+                letters += phoneme[:-1].lower()
+            else:
+                letters += phoneme.lower()
+        letters += '-'
+    return letters[:-1]
+
 def split_syllables(word):
     word = word.lower()
     if word not in cmu_dict:
         return []
-    
+
     phonemes = cmu_dict[word][0]
     syllables = []
-    current_syllable = ''
+    current_syllable = []
 
     for phoneme in phonemes:
-        current_syllable += phoneme
+        current_syllable.append(phoneme)
         if is_vowel_sound(phoneme):
             syllables.append(current_syllable)
-            current_syllable = ''
+            current_syllable = []
 
     if current_syllable:
         syllables[-1] += current_syllable
 
-    return syllables
+    return phonemes_to_letters(syllables)
+
 
 def app():
     st.title("Syllables Generator with CMU Pronouncing Dictionary")
