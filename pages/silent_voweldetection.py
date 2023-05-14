1b7184b3fa76d974001951c488e22ba776dc045a
Jude Sajith <judesajith.aj@gmail.com>
2023-05-15 01:19:30 +0530

    sss

--- pages/silent_voweldetection.py
+++ pages/silent_voweldetection.py
@@ -1,19 +1,29 @@
 import re
 import streamlit as st
 
+
 def detect_silent_vowels(word):
     silent_vowels = []
-    # Check for silent 'e' at the end of words
+
+    # Silent 'e' at the end of words
     if len(word) > 1 and word[-1] == 'e':
         silent_vowels.append('e')
 
-    # Check for silent 'a' in 'bread' pattern
-    if 'a' in word and re.search(r'ea[^u]', word):
+    # Silent 'a' in 'bread' pattern or 'each' pattern or 'aisle' pattern or 'boar' pattern
+    if 'a' in word and (re.search(r'ea[^u]', word) or re.search(r'aisle', word) or re.search(r'oa[^u]', word)):
         silent_vowels.append('a')
 
-    # Check for silent 'o' in 'people' pattern
-    if 'o' in word and re.search(r'eo[^u]', word):
+    # Silent 'o' in 'people' pattern or 'country' pattern
+    if 'o' in word and (re.search(r'eo[^u]', word) or re.search(r'ou[^u]', word)):
         silent_vowels.append('o')
+
+    # Silent 'i' in 'business' pattern or 'cushion' pattern
+    if 'i' in word and (re.search(r'business', word) or re.search(r'io[^u]', word)):
+        silent_vowels.append('i')
+
+    # Silent 'u' in 'guess' pattern or 'guide' pattern
+    if 'u' in word and (re.search(r'gue[^u]', word) or re.search(r'gui[^u]', word)):
+        silent_vowels.append('u')
 
     return silent_vowels
 
